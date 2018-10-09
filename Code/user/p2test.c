076b27a283967703d32f40c9035ce0fa62951fed
Asami Tachibana <atachibana2612@gmail.com>
2018-10-09 07:50:07 -0400

    Changes to p2tester and added a quick compile script.

--- Code/user/p2test.c
+++ Code/user/p2test.c
@@ -5,20 +5,29 @@
 #include "pstat.h"
 int main(int argc, char *argv[])
 {
-        int x = settickets(15);
-        struct pstat *stat1;
-        stat1 = malloc(sizeof(*stat1));
-        int y = getpinfo(stat1);
-        int counter = 0;
-        for(counter = 0; counter < NPROC; counter++) {
-          if (stat1->pid[counter] > 0) {
-                printf(1, "-------\n");
-                printf(1, "PID: %d\n", stat1->pid[counter]);
-                printf(1, "in-use: %d\n", stat1->inuse[counter]);
-                printf(1, "ticks: %d\n", stat1->ticks[counter]);
-                printf(1, "tickets: %d\n", stat1->tickets[counter]);
+        int x = settickets(19); //declare static ticket casting
+        int pida;
+
+        if (pida == 0) {
+          pida = fork();
+          int counter = 0;
+          struct pstat *stat1;
+          stat1 = malloc(sizeof(*stat1));
+          int y = getpinfo(stat1);
+          for(counter = 0; counter < NPROC; counter++) {
+            if (stat1->pid[counter] > 0) {
+                  printf(1, "-------\n");
+                  printf(1, "PID: %d\n", stat1->pid[counter]);
+                  printf(1, "in-use: %d\n", stat1->inuse[counter]);
+                  printf(1, "ticks: %d\n", stat1->ticks[counter]);
+                  printf(1, "tickets: %d\n", stat1->tickets[counter]);
+                }
+          }
+          pida=1;
         }
+
+        if (pida > 0) {
+          pida = wait();
+          exit();
         }
-        y = y+0;
-        exit();
 }
