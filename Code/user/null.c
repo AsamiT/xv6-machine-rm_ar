2b431ba40cdf3346f08f9a64422da8192195e7fc
Jamie <atachibana2612@gmail.com>
2018-10-19 19:58:20 -0400

    Added null reference application, which works...

---
+++ Code/user/null.c
@@ -0,0 +1,23 @@
+/**
+
+  null.c
+  @author: Robert Maloy
+  @date: 10/19/2018
+  @desc: This program is intended to invoke a null pointer dereferencing on the XV6 Operating system.
+
+  **/
+
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+	int *ptr; //pointer declaration
+	int deref; //deference integer
+	printf(1,"code %p\n", main);
+	printf(1, "stack %p\n", &ptr);
+	printf(1, "heap %p\n", ptr);
+	deref = 3;
+	ptr = deref;
+	*ptr = 0;
+	return 0;
+}
